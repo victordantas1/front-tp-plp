e0f003b10a4f9a24a1e8ea77d1da3739d6ffe003
victordantas1 <victorr.dantas@gmail.com>
2024-12-07 11:45:56 -0400

    Adiciona home-forms component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {HomeFormsComponent} from './home-forms/home-forms.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeFormsComponent }
+];
---
+++ src/app/home-forms/home-forms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-forms',
+  imports: [],
+  templateUrl: './home-forms.component.html',
+  styleUrl: './home-forms.component.css'
+})
+export class HomeFormsComponent {
+
+}

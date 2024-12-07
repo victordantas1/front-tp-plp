ac48227abe0ea83d8beada0e9608b741f9f3a137
Letícia Araújo <leticiasdarj@gmail.com>
2024-12-07 15:17:42 -0400

    Alinha input file

--- src/app/home-forms/home-forms.component.ts
+++ src/app/home-forms/home-forms.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-import {FormGroup, ReactiveFormsModule} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home-forms',
   templateUrl: './home-forms.component.html',
   imports: [
-    ReactiveFormsModule
   ],
   styleUrls: ['./home-forms.component.css']
 })
 export class HomeFormsComponent {
-  form: FormGroup;
-  file: File | null = null;
 
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
@@ -20,20 +15,6 @@ export class HomeFormsComponent {
       const files = Array.from(input.files);
       console.log('Arquivos selecionados:', files);
       // Adicione lógica para manipular os arquivos aqui
-    }
-  }
-
-  onSubmit() {
-    if (this.file) {
-      const formData = new FormData();
-      formData.append('file', this.file);
-
-      this.http.post('https://sua-api.com/upload', formData).subscribe(
-        (response) => console.log('Upload com sucesso!', response),
-        (error) => console.error('Erro no upload', error)
-      );
-    } else {
-      console.error('Nenhum arquivo selecionado');
     }
   }
 }

ded179566be2c83a57d5b76f9641cdf2a20c2d26
victordantas1 <victorr.dantas@gmail.com>
2024-12-08 13:38:12 -0400

    Cria componente de resposta

---
+++ src/app/reponse-page/reponse-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reponse-page',
+  imports: [],
+  templateUrl: './reponse-page.component.html',
+  styleUrl: './reponse-page.component.css'
+})
+export class ReponsePageComponent {
+
+}

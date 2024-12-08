0ae97d7a48bfc0f3af315ee2e11df8ad6e72b48c
victordantas1 <victorr.dantas@gmail.com>
2024-12-08 13:54:19 -0400

    Altera submissao dos arquivos

--- src/app/home-forms/home-forms.component.ts
+++ src/app/home-forms/home-forms.component.ts
@@ -1,39 +1,53 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; // Importando apenas o HttpClient
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home-forms',
-  standalone: true, // Para componentes standalone
+  standalone: true,
   templateUrl: './home-forms.component.html',
   styleUrls: ['./home-forms.component.css']
 })
 export class HomeFormsComponent {
+  selectedFiles: File[] = []; // Array para armazenar os arquivos selecionados
+
   constructor(private http: HttpClient) {}
 
   // Manipulador do evento de seleção de arquivos
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files) {
-      const files = Array.from(input.files);
-      console.log('Arquivos selecionados:', files);
+      this.selectedFiles = Array.from(input.files); // Armazenando os arquivos selecionados
+      console.log('Arquivos selecionados:', this.selectedFiles);
+    }
+  }
+  // Método para submeter os arquivos
 
-      // Chama o método para fazer upload de arquivos
-      this.uploadFiles(files).subscribe({
-        next: (response: any) => {  // Defina o tipo da resposta, geralmente `any` ou um tipo específico de resposta da sua API
+  onSubmit(event: Event): void {
+    event.preventDefault(); // Impede o comportamento padrão de envio do formulário
+
+    console.log('forms enviado');
+
+    if (this.selectedFiles.length > 0) {
+      console.log('Enviando arquivos: ', this.selectedFiles);
+      this.uploadFiles(this.selectedFiles).subscribe({
+        next: (response: any) => {  // Defina o tipo da resposta
           console.log('Resposta da API:', response);
         },
-        error: (error: any) => {  // Defina o tipo do erro, geralmente `any` ou um tipo específico de erro da sua API
+        error: (error: any) => {  // Defina o tipo do erro
           console.error('Erro ao enviar arquivos:', error);
+          console.log('Erro ao chamar a API:', error);
         },
         complete: () => {
           console.log('Envio concluído.');
         }
       });
+    } else {
+      console.log('Nenhum arquivo selecionado.');
     }
   }
 
-  // Metodo para enviar os arquivos via POST
+  // Método para enviar os arquivos via POST
   uploadFiles(files: File[]): Observable<any> {
     const formData = new FormData();
     files.forEach(file => {
@@ -44,4 +58,6 @@ export class HomeFormsComponent {
     return this.http.post('http://localhost:3000/upload', formData);
   }
 }
+
+
 

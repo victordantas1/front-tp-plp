8175fd57f8f23e95803a5af0dfdb5b57b9df5c67
victordantas1 <victorr.dantas@gmail.com>
2024-12-08 20:33:39 -0400

    Adiciona resposta ao html

--- src/app/reponse-page/reponse-page.component.ts
+++ src/app/reponse-page/reponse-page.component.ts
@@ -9,21 +9,20 @@ import {LogInfoService} from "../log-info.service";
 })
 export class ReponsePageComponent implements OnInit {
   private apiUrl = 'http://localhost:3000/upload'; // Substitua pela URL da API
-  private apiResponse: any;
+  protected apiResponse: any;
   constructor(private http: HttpClient, private logInfoService: LogInfoService) { }
 
   ngOnInit(): void {
     this.apiResponse = this.logInfoService.getResponse()
+    console.log("Dados recebidos: ", this.apiResponse);
   }
 
   carregarDados(): void {
     this.http.get<any>(this.apiUrl).subscribe({
       next: (data) => {
-        this.infoLog = data;
       },
       error: (error) => {
         console.error('Erro ao carregar os dados:', error);
-        this.erros = 'Erro ao obter dados da API.';
       },
       complete: () => {
         console.log('Requisição completa.');

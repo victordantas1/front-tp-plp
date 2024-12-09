512f36bb1164163533e5b2d0c884580ef0859bc6
Letícia Araújo <leticiasdarj@gmail.com>
2024-12-08 21:34:17 -0400

    altera csv

--- src/app/reponse-page/reponse-page.component.ts
+++ src/app/reponse-page/reponse-page.component.ts
@@ -10,7 +10,9 @@ import {LogInfoService} from "../log-info.service";
 export class ReponsePageComponent implements OnInit {
   private apiUrl = 'http://localhost:3000/upload'; // Substitua pela URL da API
   protected apiResponse: any;
-  constructor(private http: HttpClient, private logInfoService: LogInfoService) { }
+
+  constructor(private http: HttpClient, private logInfoService: LogInfoService) {
+  }
 
   ngOnInit(): void {
     this.apiResponse = this.logInfoService.getResponse()
@@ -32,13 +34,43 @@ export class ReponsePageComponent implements OnInit {
 
   exportCSV(): void {
     console.log('Exportando CSV...');
-    // Implementar lógica de exportação
+
+    if (!this.apiResponse || !this.apiResponse['files'] || this.apiResponse['files'].length === 0) {
+      console.error('Nenhum dado para exportar');
+      return;
+    }
+
+    // Obter todas as chaves únicas de todos os objetos dentro de 'files'
+    const allKeys: string[] = Array.from(new Set(this.apiResponse['files'].flatMap((file: any) => Object.keys(file))));
+
+    const csvRows: string[] = [];
+
+    // Adicionar cabeçalho (todas as chaves)
+    csvRows.push(allKeys.join(','));
+
+    // Adicionar dados das linhas
+    for (const row of this.apiResponse['files']) {
+      const values = allKeys.map((key: string) => {
+        const value = row[key] !== undefined ? row[key] : '';  // Usar valor vazio se a chave não existir
+        return `"${value}"`;  // Adicionar aspas para lidar com vírgulas nos valores
+      });
+      csvRows.push(values.join(','));
+    }
+
+    // Criar o conteúdo do CSV
+    const csvContent = csvRows.join('\n');
+
+    // Criar um blob para download
+    const blob = new Blob([csvContent], {type: 'text/csv;charset=utf-8;'});
+    const link = document.createElement('a');
+    if (link.download !== undefined) {
+      const url = URL.createObjectURL(blob);
+      link.setAttribute('href', url);
+      link.setAttribute('download', 'dados.csv');
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
   }
 }
-
-
-
-
-
-
-
